e9933b7a82b4331898d6f910d22ef9b2f31b684e
FREDERIC <kannif@ymail.com>
2020-09-29 21:10:00 +0400

    creation vue index coursecontroller  et appel des slots depuis le layout

---
+++ resources/js/Pages/Courses/index.vue
@@ -0,0 +1,20 @@
+<template>
+<app-layout>
+    <template slot='header'>
+
+        liste des formations
+    </template>
+    <h1>coucou</h1>
+</app-layout>
+</template>
+
+<script>
+import AppLayout from './../../Layouts/AppLayout'
+
+export default {
+    components: {
+        AppLayout,
+    }
+
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/courses',  'App\Http\Controllers\CourseController@index');
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
